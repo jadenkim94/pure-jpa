f74a8a8a350f66bb7a45b8b02d26d0bb79269df0
jaden94 <jonghaha7@gmail.com>
2023-07-26 16:47:06 +0900

    버전 호환

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.hibernate:hibernate-entitymanager:4.3.10.Final")
+    implementation("org.hibernate:hibernate-entitymanager:5.5.7.Final")
     implementation("com.h2database:h2:2.1.214")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
--- src/main/java/ch4/entity/Main.java
+++ src/main/java/ch4/entity/Main.java
@@ -1,7 +1,14 @@
 package ch4.entity;
 
+import org.hibernate.jpa.boot.spi.EntityManagerFactoryBuilder;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
     public static void main(String[] args) {
+
 
     }
 }
